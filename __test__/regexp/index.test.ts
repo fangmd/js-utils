49409b39840310337324f1d5a00915011d01f204
fangmingdong <fangmingdong@58.com>
2021-09-24 11:00:27 +0800

    docs: Auto commit by fe-cli.

--- __test__/regexp/index.test.ts
+++ __test__/regexp/index.test.ts
@@ -19,7 +19,9 @@ test('isDesensitizationPhone', () => {
 
 test('removeRichTextReserveWrap', () => {
   let content = `<div>出租包河百脑<strong>汇出租</strong>包河百脑汇1 </div><p>出租包河百脑汇2</p><p>出租包河百脑汇3</p><p>出租包水电费水电费123123河百脑汇4</p><p>出租包河百脑汇5</p><p><br></p><p style="position: absolute; width: 1px; height: 1px; overflow: hidden; left: -1000px; white-space: nowrap; top: 71px;">出租包河百脑汇6</p><p style="position: absolute; width: 1px; height: 1px; overflow: hidden; left: -1000px; white-space: nowrap; top: 71px;"><br></p><p style="position: absolute; width: 1px; height: 1px; overflow: hidden; left: -1000px; white-space: nowrap; top: 71px;">出租包河百脑汇7</p><p style="position: absolute; width: 1px; height: 1px; overflow: hidden; left: -1000px; white-space: nowrap; top: 71px;"><br></p><p style="position: absolute; width: 1px; height: 1px; overflow: hidden; left: -1000px; white-space: nowrap; top: 129px;"><span style="white-space: normal;">出租包河百脑汇8</span></p><p style="position: absolute; width: 1px; height: 1px; overflow: hidden; left: -1000px; white-space: nowrap; top: 129px;"><span style="white-space: normal;"><br></span></p>`
-  expect(removeRichTextReserveWrap(content)).toBe(
+  let ret = removeRichTextReserveWrap(content)
+  // console.log(ret);
+  expect(ret).toBe(
     `出租包河百脑汇出租包河百脑汇1 \n出租包河百脑汇2\n出租包河百脑汇3\n出租包水电费水电费123123河百脑汇4\n出租包河百脑汇5\n\n\n出租包河百脑汇6\n\n\n出租包河百脑汇7\n\n\n出租包河百脑汇8\n\n\n`
   )
 })
